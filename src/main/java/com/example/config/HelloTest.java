fb1fc9a45cdf2a7cd7d2a4e650227dc69fc51e9b
Surjeet Mohanty <surjeetmohanty2019@gmail.com>
2020-04-29 12:59:34 +0530

    HelloWorld local changed

--- src/main/java/com/example/config/HelloTest.java
+++ src/main/java/com/example/config/HelloTest.java
@@ -5,6 +5,9 @@ public class HelloTest {
 	public void m() {
 		for (int i = 0; i < 10; i++) {
 			System.out.println("Future 2 Test..");
+			for(int j=0;j<100;j++) {
+				System.out.println("Local changes");
+			}
 		}
 		while(true){
 			System.out.println("Infinite Loop");
